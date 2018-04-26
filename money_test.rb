c4961fa33ad4fdfc26132c69cb97d19d1dbef26d
Gaston Ramos <ramos.gaston@gmail.com>
2018-04-26 11:24:52 -0300

    Add Money.Config singleton method.
    add Money#convert_to.

--- money_test.rb
+++ money_test.rb
@@ -1,4 +1,16 @@
 class Money
+  class Config < Struct.new(:default_currency, :conversions)
+  end
+
+  class << self
+    attr_accessor :config
+  end
+
+  def self.configure
+    @config = Config.new
+    yield(@config)
+  end
+
   attr_accessor :amount, :currency
 
   def initialize(amount, currency)
@@ -10,7 +22,8 @@ class Money
     "#{"%0.2f" % @amount} #{@currency}"
   end
 
-  def self.configure
+  def convert_to(currency)
+    ( amount * self.class.config.conversions[currency] ).round(2)
   end
 end
 
@@ -22,6 +35,11 @@ Money.configure do |config|
   }
 end
 
+test "Money class configuration" do
+  convert_hash = {'USD' => 1.11, 'Bitcoin' => 0.0047}
+  assert convert_hash == Money.config.conversions
+end
+
 test "Instance a new money obj" do
   fifty_eur = Money.new(50, 'EUR')
 
@@ -30,3 +48,7 @@ test "Instance a new money obj" do
   assert "50.00 EUR" == fifty_eur.inspect
 end
 
+test "Money#convert_to" do
+  fifty_eur = Money.new(50, 'EUR')
+  assert 55.50 == fifty_eur.convert_to('USD')
+end
